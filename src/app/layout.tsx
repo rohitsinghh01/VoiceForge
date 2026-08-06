7d64f59b5145afef5dada77fff39e0284b0a762f
rohitsinghh01 <singh.rohitraj122@gmail.com>
2026-08-06 15:05:10 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter,Geist_Mono } from "next/font/google";
+import { Toaster } from "sonner";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "VoiceForge",
+  description: "AI-powered text-to-speech and voice cloning platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+        <Toaster/>
+      </body>
+    </html>
+  );
+}
